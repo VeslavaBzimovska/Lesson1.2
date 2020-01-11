f758682b29ecc9eadf1069c778301a2852240a39
VeslavaBzimovska <wieslava.b@gmail.com>
2020-01-11 12:35:30 +0200

    List of grades

--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -10,6 +10,21 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            int[] listOfgrades = new int[3];
+            Console.WriteLine("Please enter grades: ");
+            for (int i = 0; i < listOfgrades.Length; i++)
+            {
+                Console.WriteLine("Please enter next grade: ");
+                int grade = Convert.ToInt32(Console.ReadLine());
+                listOfgrades[i] = grade;
+            }
+            Console.WriteLine("");
+            foreach (var item in listOfgrades)
+            {
+                Console.Write(item + ", " + item);
+
+            }
+            Console.ReadLine();
         }
     }
 }
